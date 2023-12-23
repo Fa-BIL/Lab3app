46c115d16205f1fd46c49cb1c30bc30cd39e5ea7
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-23 19:09:48 +0400

    first commit

---
+++ interface.py
@@ -0,0 +1,8 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+import sys
+import os
+
+from goal_5 import Iterator
+
---
+++ main_window.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+from PyQt5 import QtWidgets
+
+from goal_5 import Iterator
+import goal_1
+import goal_2
+import goal_3
+
