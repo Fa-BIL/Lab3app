569c640e0b965809b97aff354c1c07d8c06252b3
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-23 19:13:14 +0400

    ScrollLabel

--- main_window.py
+++ main_window.py
@@ -8,3 +8,16 @@ import goal_1
 import goal_2
 import goal_3
 
+class ScrollLabel(QtWidgets.QScrollArea):
+    def __init__(self, *args, **kwargs) -> None:
+        QtWidgets.QScrollArea.__init__(self, *args, **kwargs)
+        self.setWidgetResizable(True)
+        text = QtWidgets.QWidget(self)
+        self.setWidget(text)
+        lay = QtWidgets.QVBoxLayout(text)
+        self.label = QtWidgets.QLabel(text)
+        self.label.setWordWrap(True)
+        lay.addWidget(self.label)
+
+    def setText(self, text: str) -> None:
+        self.label.setText(text)
